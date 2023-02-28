2e0d7478220ae11a49c5aa1032725acb5ab770fe
Ben Buzard <250144benjamin@gmail.com>
2023-02-27 22:46:53 -0800

    Add skills page with skills

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-
 	import Toast from "$lib/components/Toast.svelte";
+    import Card from "$lib/components/Card.svelte";
 
     let toast: Toast
 
 </script>
 
-home
+<div class="flex justify-center items-center p-10">
+    <Card>
+        <h2 class="card-title">About Me</h2>
+        <p>I program as a hobby, and play the clarinet in the CVHS Wind Ensemble and Symphony.</p>
+    </Card>
+</div>
 
 <Toast bind:this={toast} />
---
+++ src/routes/skills/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import CollapsableCard from "$lib/components/CollapsableCard.svelte";
+	import IconLink from "$lib/components/IconLink.svelte";
+    import Link from "$lib/components/Link.svelte";
+</script>
+
+<div class="flex justify-center items-center p-10">
+    <div class="grid gap-4 grid-rows-4 grid-cols-6">
+        <IconLink href="https://typescriptlang.org" icon="typescript"></IconLink>
+        <IconLink href="https://tailwindcss.com" icon="tailwindcss"></IconLink>
+        <IconLink href="https://svelte.dev" icon="svelte"></IconLink>
+        <IconLink href="https://kotlinlang.org" icon="kotlin"></IconLink>
+        <IconLink href="https://rust-lang.org" icon="rust"></IconLink>
+        <IconLink href="https://apple.com" icon="apple"></IconLink>
+    </div>
+</div>
