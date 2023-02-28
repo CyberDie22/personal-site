76beb0b3dbca763f86552e22f3e8564a11d4a20e
Ben Buzard <250144benjamin@gmail.com>
2023-02-27 22:45:37 -0800

    Adds a component that makes a square icon button with devicons

---
+++ src/lib/components/IconLink.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let icon: string;
+    export let href: string;
+</script>
+
+<a href={href} target="_blank" rel="noreferrer" class="bg-base-300 border border-base-300 hover:bg-opacity-90 hover:border-white hover:border-opacity-50 hover:border p-4 rounded-lg"><i class="devicon-{icon}-plain colored text-5xl"></i></a>
