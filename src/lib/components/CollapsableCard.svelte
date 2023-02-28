675cec94b2cd960add807aeb105fede2fd4c4969
Ben Buzard <250144benjamin@gmail.com>
2023-02-27 22:44:59 -0800

    Add component that makes a collapsable card

---
+++ src/lib/components/CollapsableCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let title: string;
+    export let titleiconclass: string = "";
+</script>
+
+<div class="collapse card bg-base-300">
+    <input type="checkbox" /> 
+    <div class="collapse-title text-xl font-medium text-center p-4">
+        {#if titleiconclass != ""} <i class={titleiconclass}></i> {/if} {title}
+    </div>
+    <div class="collapse-content">
+        <slot />
+    </div>
+</div>
