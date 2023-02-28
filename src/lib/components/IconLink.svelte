91d7153d31f579690800cb8b80c9a880bc847d96
Ben Buzard <250144benjamin@gmail.com>
2023-02-27 22:48:20 -0800

    Add shadow to IconLink

--- src/lib/components/IconLink.svelte
+++ src/lib/components/IconLink.svelte
@@ -3,4 +3,4 @@
     export let href: string;
 </script>
 
-<a href={href} target="_blank" rel="noreferrer" class="bg-base-300 border border-base-300 hover:bg-opacity-90 hover:border-white hover:border-opacity-50 hover:border p-4 rounded-lg"><i class="devicon-{icon}-plain colored text-5xl"></i></a>
+<a href={href} target="_blank" rel="noreferrer" class="bg-base-300 border border-base-300 hover:bg-opacity-90 hover:border-white hover:border-opacity-50 hover:border p-4 rounded-lg shadow-xl"><i class="devicon-{icon}-plain colored text-5xl"></i></a>
