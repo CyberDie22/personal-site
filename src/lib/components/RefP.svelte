a63aebe4883818bc2d40c7fc5411b2ac32b35ba0
Ben Buzard <250144benjamin@gmail.com>
2023-03-07 12:09:03 -0800

    Add 'RefP' component that adds a slight underline to add a reference to some text

---
+++ src/lib/components/RefP.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let href: string;
+</script>
+
+<a href={href} target="_blank" rel="noreferrer" class="underline decoration-white/30 hover:decoration-white/60">
+    <slot />
+</a>
