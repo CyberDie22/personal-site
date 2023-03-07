f25d2d1a9c72b8021b5da5c426d10ccced1b9417
Ben Buzard <250144benjamin@gmail.com>
2023-03-07 09:56:20 -0800

    Add clazz attribute to Card to allow adding your own classes to the card

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,4 +1,8 @@
-<div class="card shadow-xl bg-base-300 w-fit">
+<script lang="ts">
+    export let clazz: string = "";
+</script>
+
+<div class="card shadow-xl bg-base-300 w-fit {clazz}">
     <div class="card-body">
         <slot />
     </div>
