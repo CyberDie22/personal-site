f86655f6f94d5d2741b6c002f989986ef7695e1c
Ben Buzard <250144benjamin@gmail.com>
2023-03-07 14:59:09 -0800

    Change default size of card from w-fit to w-2xl (42rem)

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -2,7 +2,7 @@
     export let clazz: string = "";
 </script>
 
-<div class="card shadow-xl bg-base-300 w-fit {clazz}">
+<div class="card shadow-xl bg-base-300 min-w-[42rem] max-w-2xl {clazz}">
     <div class="card-body">
         <slot />
     </div>
