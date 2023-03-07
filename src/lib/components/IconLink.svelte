0143129d3326ace18c82ffe16f6e20d03a495a90
Ben Buzard <250144benjamin@gmail.com>
2023-03-07 14:56:42 -0800

    Add 'clazz' property to LinkCard for adding classes to it

--- src/lib/components/IconLink.svelte
+++ src/lib/components/IconLink.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     export let icon: string;
     export let href: string;
-    export let hovered: (name: string) => string;
+    export let hovered: (name: string) => void;
+    export let unhover: () => void;
 </script>
 
-<a href={href} target="_blank" rel="noreferrer" on:focus={() => hovered(icon)} on:mouseover={() => hovered(icon)} on:focusout={() => hovered("none")} on:mouseleave={() => hovered("none")} class="bg-base-300 border border-base-300 hover:bg-opacity-90 hover:border-white hover:border-opacity-50 hover:border p-4 rounded-lg shadow-xl"><i class="devicon-{icon}-plain colored text-5xl"></i></a>
+<a href={href} target="_blank" rel="noreferrer" on:focus={() => hovered(icon)} on:mouseover={() => hovered(icon)} on:focusout={() => unhover()} on:mouseleave={() => unhover()} class="bg-base-300 border border-base-300 hover:bg-opacity-90 hover:border-white hover:border-opacity-50 hover:border p-4 rounded-lg shadow-xl"><i class="devicon-{icon}-plain colored text-5xl"></i></a>
--- src/lib/components/LinkCard.svelte
+++ src/lib/components/LinkCard.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
     export let href: string;
+    export let clazz: string = "";
 </script>
-<a href={href} class="card shadow-xl bg-base-300 w-fit border border-base-300 hover:bg-opacity-90 hover:border-white hover:border-opacity-50 hover:border">
+<a href={href} class="card shadow-xl bg-base-300 border border-base-300 hover:bg-opacity-90 hover:border-white hover:border-opacity-50 hover:border min-w-[42rem] max-w-2xl {clazz}">
     <div class="card-body">
         <slot />
     </div>
